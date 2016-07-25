48281f9af30792d662c24e178a78523508d680ab
jiangzy27 <jiangzy27@gmail.com>
2016-07-25 17:09:05 +0800

    mixin

--- react/mixin/mixin.js
+++ react/mixin/mixin.js
@@ -13,6 +13,8 @@ var helloMixin = {
 
 var MyTitle = React.createClass({
     mixins:[helloMixin],//打入
+    //如果设置相同的 Prop 和 State则会报错。
+    //如果mixin有周期函数，则先加载，如果多个mixin都有周期函数，按照从左到右的顺序加载。
     componentDidMount: function () {
         console.log('from MyTitle');
     },
