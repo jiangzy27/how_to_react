8c15696ab2f859cc9d60ff61c9c6e0041fd92399
jiangzy27 <jiangzy27@gmail.com>
2016-07-25 15:39:01 +0800

    actions

---
+++ react/reflux/actions/actions.js
@@ -0,0 +1,63 @@
+//引入包
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Reflux from 'reflux';
+import $ from 'jquery';
+//创建多个actions的方法
+var  TodoActions = Reflux.createActions(['addItem','deleteItem']);
+//创建store，监控多个action方法
+var TodoStore = Reflux.createStore({
+    items:["jack","tom"],//数据
+    init:function(){
+        this.listenTo(TodoActions.addItem,'addItem');//这个类似backbone的写法了。
+        this.listenTo(TodoActions.deleteItem,'deleteItem');
+
+    },
+    addItem:function(model){
+        console.log('*********');
+        this.items.push(model);
+        this.trigger(this.items);//更新items
+        console.log(this.items);
+    },
+    deleteItem:function(model){
+        console.log('@@@@@@@@@@');
+        var index = this.items.indexOf(model);
+        if (index > -1) {
+            this.items.splice(index, 1);
+            this.trigger(this.items);//更新items
+        }
+        console.log(this.items);
+
+    }
+});
+//创造一个view
+var TodoComponent = React.createClass({
+
+    //监听store
+    mixins:[Reflux.listenTo(TodoStore,'onStatusChange')],
+    getInitialState:function(){
+        return {list:[]};
+    },
+    //监听到store发生变化后触发
+    onStatusChange:function(){
+        this.setState({list:TodoStore.items});
+    },
+    render:function(){
+        return (
+            <div>
+                {this.state.list.map(function(item){
+                    return <p>{item}</p>
+                })}
+
+            </div>
+
+
+        );
+    }
+});
+ReactDOM.render(<TodoComponent />,document.getElementById('app'));
+//启动,单项数据流动
+TodoActions.addItem('newperson');
+setTimeout(function(){
+    TodoActions.deleteItem('jack');
+},2000);
---
+++ react/reflux/actions/webpack.config.js
@@ -0,0 +1,23 @@
+//分开打包，将react等文件，都打入common.js里面，单独引入，这样就避免了每个组件过大的问题。
+var webpack = require('webpack');
+module.exports = {
+    entry:{
+        'bundle':'./entry.js',//键是最终打包后的名字
+        vendors: ['react','react-dom','reflux']
+    },//指定打包入口文件，每有一个键值对，就是一个入口文件。
+    output:{
+        path:__dirname,//定义输出文件夹
+        filename:"./dest/[name].js"
+    },
+    module:{
+        loaders:[//定义一系列加载器。
+
+            {test:/\.js?$/,loader:'babel-loader',exclude:/node_modules/,query:{compact:false,presets:['es2015','react']}}
+        ]
+
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'common.js')
+    ]
+
+};
