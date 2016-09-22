3a3a089579bde62ef1403ff42f00f702668cc106
jiangzy27 <jiangzy27@gmail.com>
2016-09-22 13:57:10 +0800

    mixin

--- react/mixin/mixin.js
+++ react/mixin/mixin.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
+let IntervalEnhance = myComponent => {
+    //声明一个内部类
+    class ES6_Mixin extends Component {
 
+        componentWillMount() {
+            console.log("渲染前");
+            this.timer = setInterval(function(){
+                console.log('不断打印中.....');
+            },500);
+        }
+        componentWillUnmount(){
+            console.log("销毁啦！");
+            clearInterval(this.timer);
+        }
+        //这里是重点
+        render(){
+            return (<myComponent {...this.props} {...this.state} />);
+        }
+    }
+
+    return ES6_Mixin;
+
+};
+
+class MyTest extends Component{
+    render(){
+        return (<div>我出生了！</div>);
+    }
+}
+
+var enhancedTest = IntervalEnhance(MyTest);
+ReactDOM.render(<enhancedTest />,document.getElementById('app'));
+
+/*
 //声明一个mixin,注意这是一个对象！
 var helloMixin = {
     componentDidMount: function () {
@@ -27,4 +60,4 @@ ReactDOM.render(
     <MyTitle />,
     document.getElementById('app')
 
-);
+);*/
