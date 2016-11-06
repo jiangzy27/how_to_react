c819f0981dc280d316f42d975cf6b492ca668a72
jiangzy27 <jiangzy27@gmail.com>
2016-11-06 15:34:04 +0800

    表单验证

--- react/example/checkform/checkform.js
+++ react/example/checkform/checkform.js
@@ -1,8 +1,11 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Form, Input, Button,Select} from 'antd';
+import $ from 'jquery';
+import { Form, Input, Button,Select,DatePicker,Radio,Checkbox } from 'antd';
 const FormItem = Form.Item;
-
+Mock.mock('http://ajax.data.com',{
+    'myName':'樱木花道'
+});
 const NormalLoginForm = Form.create()(React.createClass({
   handleSubmit(e) {
     e.preventDefault();
@@ -12,9 +15,28 @@ const NormalLoginForm = Form.create()(React.createClass({
       if (err) {
         return;
       }
-
       console.log('获取的值：', values);
     });
+  },
+  checkBirthday(rule, value, callback){
+    if (value && value.valueOf() >= Date.now()) {
+      callback(new Error("你不可能生于未来吧"));
+    } else {
+      callback();
+    }
+  },
+  ajaxCheck(rules,value,callback){
+        $.ajax({
+            url:'http://ajax.data.com',
+            dataType:'json',
+            success:function(data){
+                if(value == data['myName']){
+                    callback(new Error("樱木花道已注册"));
+                }else{
+                    callback();
+                }
+            }
+        });
   },
   render() {
     const { getFieldDecorator } = this.props.form;
@@ -22,27 +44,66 @@ const NormalLoginForm = Form.create()(React.createClass({
       <Form onSubmit={this.handleSubmit}>
         <FormItem>
           {getFieldDecorator('userName', {
-            rules: [{ required: true, message: '用户名不能为空!' }],
+            rules:
+             [
+                { required: true, type:'string',message: '用户名不能为空' },
+                { min: 3, type:'string',message: '用户名不能少于3位' },
+                { max: 6, type:'string',message: '用户名不能多于6位' },
+                { pattern:/^[\u2E80-\u9FFF]+$/,type:'string',message: '用户名必须为汉字' },
+                {validator: this.ajaxCheck}
+
+             ]
           })(
             <Input  placeholder="请输入用户名" />
           )}
         </FormItem>
         <FormItem>
           {getFieldDecorator('selectName',{
-            rules:[{ required:true, message:'select必选一项' }], 
+            rules:[{ required:true, type:'string',message:'请选择国家'}], 
           })(
             <Select>
               <Select.Option value="">请选择</Select.Option>
-              <Select.Option value="2">JACK</Select.Option>
-              <Select.Option value="0" disabled>不能选</Select.Option>
-              <Select.Option value="3">TOM</Select.Option>
+              <Select.Option value="USA">美国</Select.Option>
+              <Select.Option value="Korea" disabled>韩国</Select.Option>
+              <Select.Option value="Japan">日本</Select.Option>
             </Select>
           )}
-        </FormItem>        
+        </FormItem> 
+
+        <FormItem>
+          {getFieldDecorator('radioChoise',{
+            rules:[{required:true,type:'string',message:'请选择性别'}]
+          })(
+            <Radio.Group>
+               <Radio value="male">男</Radio>
+              <Radio value="female">女</Radio>
+            </Radio.Group>  
+          )}
+        </FormItem>
+        <FormItem>
+         {getFieldDecorator('interest')(
+            <Checkbox.Group options={['读书', '跑步', '网游']} />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('birthday', {
+            rules: [
+              {
+                required: true,
+                type: 'object',
+                message: '生日不可为空',
+              }, {
+                validator: this.checkBirthday,
+              },
+            ],
+          })(
+            <DatePicker />
+          )}
+        </FormItem>
         <FormItem>
           
           <Button type="primary" htmlType="submit">
-            登录
+            提交
           </Button>
          
         </FormItem>
